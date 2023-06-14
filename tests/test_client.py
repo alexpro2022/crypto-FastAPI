e8e4253a75646930e306115d6c1028e47b488aa4
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-06-14 20:52:57 +0300

    Update tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,8 +9,9 @@ from app.client.deribit import get_tickers
 @pytest.mark.asyncio
 async def test_aiohttp_client(event_loop):
     assert event_loop is asyncio.get_running_loop()
+    before = dt.now().timestamp()
     for ticker in await get_tickers():
         assert ticker.get('result').get('instrument_name').split('-')[0] in ('BTC', 'ETH')
         assert isinstance(ticker.get('result').get('index_price'), float)
         assert isinstance(ticker.get('result').get('timestamp'), int)
-        assert dt.now().timestamp() < ticker.get('result').get('timestamp') / 1000
+        assert before < ticker.get('result').get('timestamp') / 1000
