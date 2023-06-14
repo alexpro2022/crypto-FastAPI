5d9c6a0967bab1bdafd42e39845b2961c3bb4842
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-06-14 20:49:56 +0300

    Update tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime as dt
 
 import pytest
 
@@ -12,3 +13,4 @@ async def test_aiohttp_client(event_loop):
         assert ticker.get('result').get('instrument_name').split('-')[0] in ('BTC', 'ETH')
         assert isinstance(ticker.get('result').get('index_price'), float)
         assert isinstance(ticker.get('result').get('timestamp'), int)
+        assert dt.now().timestamp() < ticker.get('result').get('timestamp') / 1000
