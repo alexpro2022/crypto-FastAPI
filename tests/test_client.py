619a9cfbc9b6841c71da73259a648571ceb73a05
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-06-15 10:24:58 +0300

    Added currency arbitrary choice

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,8 +11,8 @@ from app.client.deribit import get_tickers
 async def test_aiohttp_client(event_loop):
     assert event_loop is asyncio.get_running_loop()
     assert asyncio.iscoroutine(get_tickers())
-    before = dt.now()
-    after = before + timedelta(minutes=1)
+    before = dt.now() - timedelta(seconds=1)
+    after = dt.now() + timedelta(minutes=1)
     for ticker in await get_tickers():
         assert ticker.get('result').get('instrument_name').split('-')[0] in ('BTC', 'ETH')
         assert isinstance(ticker.get('result').get('index_price'), float)
